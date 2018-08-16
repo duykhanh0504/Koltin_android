c7a09908048abaa93df7289fe41e1f8224532060
duykhanh <khanh.tranduy@hotmail.com>
2018-08-16 14:22:07 +0700

    up

---
+++ nhanhnhuchop/app/src/main/java/com/tornado/nhanhnhuchop/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.tornado.nhanhnhuchop
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private var btnStart: Button? = null
+    private var btnHignscore: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStart = this.findViewById(R.id.start) as Button;
+        btnStart!!.setOnClickListener {
+            val intent = Intent(this, MainGame::class.java)
+            startActivity(intent);
+        }
+    }
+}
---
+++ nhanhnhuchop/app/src/main/java/com/tornado/nhanhnhuchop/Model/ItemModel.kt
@@ -0,0 +1,9 @@
+package com.tornado.nhanhnhuchop.Model
+
+class item(
+        val id: Int,
+        val answer: List<String>,
+        val question: String,
+        val rightanswer: Int
+)
+
