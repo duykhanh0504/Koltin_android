a0dc0c76cf128e2e24395bfa5d7e9e5f984a0d61
Duy khanh <kasuko.go@gmail.com>
2018-08-22 23:32:05 +0700

    up

--- nhanhnhuchop/app/src/main/java/com/tornado/nhanhnhuchop/MainGame.kt
+++ nhanhnhuchop/app/src/main/java/com/tornado/nhanhnhuchop/MainGame.kt
@@ -1,8 +1,16 @@
 package com.tornado.nhanhnhuchop
 
+import android.content.Intent
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.JsonReader
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
@@ -10,16 +18,76 @@ import com.tornado.nhanhnhuchop.Model.item
 import org.json.JSONObject
 import java.io.InputStream
 import java.io.StringReader
+import com.google.android.gms.ads.*
 
-class MainGame : AppCompatActivity() {
+class MainGame : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(p0: View?) {
+
+        val item_id = p0?.id
+        when (item_id) {
+            1 -> {
+
+            }
+            2 -> {
+
+            }
+            3 -> {
+
+            }
+            4 -> {
+
+            }
+        }
+    }
+
+    lateinit var mInterstitialAd: InterstitialAd
 
     var listquestion: List<item> = ArrayList();
+    var numberquestion: Int =0
+    var currentquestion: Int =0
+
+    var txtquestion:TextView? = null;
+    var container:FrameLayout? = null;
+
+    private var btnAnswer1: Button? = null
+    private var btnAnswer2: Button? = null
+    private var btnAnswer3: Button? = null
+    private var btnAnswer4: Button? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_game)
-        var obj = JSONObject(readJSONFromAsset());
-        listquestion = getListQuestionFromJson(obj.getString("result"));
+        val adView = findViewById(R.id.adview) as AdView
+
+         txtquestion = findViewById(R.id.question) as TextView
+         //container = findViewById(R.id.container) as FrameLayout
+
+        btnAnswer1 = findViewById(R.id.answer1) as Button
+        btnAnswer2 = findViewById(R.id.answer2) as Button
+        btnAnswer3 = findViewById(R.id.answer3) as Button
+        btnAnswer4 = findViewById(R.id.answer4) as Button
+
+        btnAnswer1!!.setOnClickListener {
+
+        }
+        btnAnswer2!!.setOnClickListener {
+
+        }
+        btnAnswer3!!.setOnClickListener {
+
+        }
+        btnAnswer3!!.setOnClickListener {
+
+        }
+
+        val adRequest = AdRequest.Builder()
+                .build()
+        adView.loadAd(adRequest)
+
+        AsyncTaskLoading().execute();
+
     }
 
     fun getListQuestionFromJson(jsonStr: String): List<item> {
@@ -40,5 +108,54 @@ class MainGame : AppCompatActivity() {
             return null
         }
         return json
+    }
+
+    fun nextquestion(){
+        txtquestion?.text = listquestion.get(currentquestion).question
+        var array = listquestion.get(currentquestion).answer
+        btnAnswer1?.text = array.get(0);
+        btnAnswer2?.text = array.get(1);
+        btnAnswer3?.text = array.get(2);
+        btnAnswer4?.text = array.get(3);
+       /* var i =0;
+        for ( temp in array) {
+            val button_dynamic = Button(this)
+            button_dynamic.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            button_dynamic.text = temp
+            val id =  currentquestion + i
+            button_dynamic.id = i
+            i++
+            button_dynamic!!.setOnClickListener(this)
+            container?.addView(button_dynamic)
+        }*/
+
+    }
+
+    inner class AsyncTaskLoading : AsyncTask<String, String, List<item>>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+        }
+
+        override fun doInBackground(vararg p0: String?): List<item> {
+
+            var Result: List<item> = ArrayList();
+            try {
+                var obj = JSONObject(readJSONFromAsset());
+                Result = getListQuestionFromJson(obj.getString("result"));
+            } catch (e: Exception) {
+
+            }
+
+            return Result
+        }
+
+        override fun onPostExecute(result: List<item>) {
+            super.onPostExecute(result)
+            listquestion = result
+            nextquestion()
+
+        }
     }
 }
